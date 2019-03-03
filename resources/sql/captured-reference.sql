0249772f0402e10aa477aef77d1a04b4bd235da9
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-03-03 13:21:51 +0100

    Captured reference :classified defined from whether article exists

--- resources/sql/captured-reference.sql
+++ resources/sql/captured-reference.sql
@@ -1,13 +1,30 @@
+-- :snip snip-select
+SELECT id,
+       reference,
+       created_at,
+       ( SELECT
+             CASE
+                WHEN EXISTS(SELECT * FROM articles WHERE id_captured_reference = cr.id)
+                     THEN True
+                ELSE False
+             END
+       ) as classified
+FROM captured_references cr
+
 -- :name create-captured-reference! :insert
 -- :doc creates a new captured-reference
 INSERT INTO captured_references
 (reference, created_at)
 VALUES (:reference, :created-at)
 
--- :name get-captured-reference :? :1
+-- :name q-get-captured-reference :? :1
 -- :doc retrieves a captured-reference given it'is id
-SELECT id, reference, created_at, FALSE as classified FROM captured_references
+:snip:select
 WHERE id = :id
+
+-- :name q-get-all-captured-references :? :*
+-- :doc retrieves all captured references
+:snip:select
 
 -- :name delete-all-captured-references :! :n
 -- :doc deletes all captured references
@@ -17,10 +34,6 @@ DELETE FROM captured_references
 UPDATE captured_references
 SET reference = :reference
 WHERE id = :id
-
--- :name get-all-captured-references :? :*
--- :doc retrieves all captured references
-SELECT id, reference, created_at, FALSE as classified FROM captured_references
 
 -- :name captured-reference-id-exists? :? :1
 SELECT :id AS res FROM captured_references WHERE id = :id
